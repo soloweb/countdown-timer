27249f41875200fa22d2216a0b9b3e2dd1259750
Tony Onodi <tony@soloweb.com>
2014-04-08 14:48:39 +0100

    Now takes British Summer Time into account.

--- countdown.js
+++ countdown.js
@@ -1,3 +1,89 @@
+function accountForBST(future) {
+    var currentlyBST,
+        futureBST,
+        sumBST,
+        milliDate,
+        adjustMilliseconds;
+
+    // check if current date is BST
+    currentlyBST = isBSTinEffect(new Date());
+
+    // check if countdown date is BST
+    futureBST = isBSTinEffect(future);
+
+    // check if BST is going to change
+    /*********************************
+
+		This gives the "truth table":
+
+		Now   | Future | return
+		-----------------------
+		0     |  1     |  -1
+		1     |  0     |   1
+		0     |  0     |   0
+		1     |  1     |   0
+
+    **********************************/
+    sumBST = currentlyBST + (futureBST * (-1));
+
+    // +/- 1 hour in milliseconds
+    adjustMilliseconds = sumBST * 3600000;
+
+    // convert the future date to milliseconds
+    milliDate = future.getTime();
+
+    // add or subtract an hour to/form future date
+    milliDate += adjustMilliseconds;
+    future.setTime(milliDate);
+
+    // return adjusted future date
+    return future;
+}
+
+/*
+  A function that determines whether BST is currently in effect in London,
+  based on the date on the user's PC, but regardless of whether the user
+  is in London, or somewhere else. BST runs between the last Sunday of
+  March, and the last Sunday of October, with a varying date each year.
+*/
+function isBSTinEffect(countTo) {
+    var d = countTo;
+
+    // Loop over the 31 days of March for the current year
+    for (var i = 31; i > 0; i--) {
+        var tmp = new Date(d.getFullYear(), 2, i);
+
+        // If it's Sunday
+        if (tmp.getDay() == 0) {
+            // last Sunday of March
+            lSoM = tmp;
+
+            // And stop the loop
+            break;
+        }
+    }
+
+    // Loop over the 31 days of October for the current year
+    for (var i = 31; i > 0; i--) {
+        var tmp = new Date(d.getFullYear(), 9, i);
+
+        // If it's Sunday
+        if (tmp.getDay() == 0) {
+            // last Sunday of October
+            lSoO = tmp;
+
+            // And stop the loop
+            break;
+        }
+    }
+
+    // 0 = DST off (GMT)
+    // 1 = DST on  (BST)
+    if (d < lSoM || d > lSoO) return 0;
+    else return 1;
+}
+
+
 // calculates remaining time and writes to DOM
 function setTime(editNode, countTo) {
     var currentTime = Date.now(),
@@ -6,10 +92,10 @@ function setTime(editNode, countTo) {
         remainingMinutes,
         remainingSeconds;
 
-    remainingTime = countTo - currentTime;
+    remainingTime = countTo - currentTime; // get time difference in milliseconds
     remainingHours = (remainingTime / 3600000).toFixed(0); // 60 * 60 * 1000
-    remainingMinutes = (remainingTime % 3600000 / 60000).toFixed(0);
-    remainingSeconds = (remainingTime % 60000 / 1000).toFixed(0);
+    remainingMinutes = ((remainingTime % 3600000) / 60000).toFixed(0);
+    remainingSeconds = ((remainingTime % 60000) / 1000).toFixed(0);
 
     editNode.innerHTML = remainingHours + ' hours ' + remainingMinutes + ' minutes ' + remainingSeconds + ' seconds ';
 }
@@ -17,12 +103,15 @@ function setTime(editNode, countTo) {
 // takes string in UTC format and returns date object
 function setUTC(dateString) {
     // set default values for UTC arguments
-    var dateArray = [1901, 0, 0, 0, 0, 0, 0],
+    var dateArray,
         utcDate,
         stringSplit,
-        separator = ',';
+        separator;
 
-
+    separator = ',';
+    // Set defaults
+    dateArray = [1901, 0, 0, 0, 0, 0, 0];
+    // comma separate string
     stringSplit = dateString.split(separator);
 
     // loop over array
@@ -31,8 +120,11 @@ function setUTC(dateString) {
         dateArray[i] = stringSplit[i]
     };
 
-    // create and set date object
-    utcDate = new Date(Date.UTC(dateArray[0], dateArray[1] - 1, dateArray[2], dateArray[3], dateArray[4], dateArray[5], dateArray[6]));
+    // create and set date object	year		month				date 		hours 			minutes  	seconds 	  millseconds
+    utcDate = new Date(Date.UTC(dateArray[0], dateArray[1] - 1, dateArray[2], dateArray[3] - 1, dateArray[4], dateArray[5], dateArray[6]));
+
+    // adjust for BST
+    utcDate = accountForBST(utcDate);
 
     return utcDate;
 }
@@ -44,9 +136,9 @@ function countdown() {
         countdownData,
         countdownTime;
 
-    countdownId = "#countdown", // selector for element
-    countdownNode = document.querySelector(countdownId), // get element
-    countdownData = countdownNode.getAttribute("data-count-date"), // get the time and date as UTC formatted string
+    countdownId = "#countdown"; // selector for element
+    countdownNode = document.querySelector(countdownId); // get element
+    countdownData = countdownNode.getAttribute("data-count-date"); // get the time and date as UTC formatted string
     countdownTime = setUTC(countdownData); // use data to create Date object
 
     // check that element exists
